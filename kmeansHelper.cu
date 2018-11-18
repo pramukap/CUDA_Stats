b0d2b19468a089c87c21b80dbf87c1d553cb5aa7
harrisonkeane <hjkeane@utexas.edu>
2018-11-18 11:18:46 -0600

    added atomicAdd for doubles

--- kmeansHelper.cu
+++ kmeansHelper.cu
@@ -57,6 +57,21 @@ __global__ void assignClass(double *distances, int* labels, int k, int i){
 
 }
 
+// PROVIDED BY NVIDIA
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                                          (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
+                        __longlong_as_double(assumed)));
+    } while (assumed != old);
+    return __longlong_as_double(old);
+}
+
 // counts has to be initialized to zeros
 __global__ void findNewCentroids(double *points, double *centroids, int* classlabels, int m, int n, int k, int* counts){
 
@@ -64,6 +79,8 @@ __global__ void findNewCentroids(double *points, double *centroids, int* classla
     int label = classlabels[x / n];
 
     atomicAdd(counts+label, 1);
+    
+    
     atomicAdd(centroids+k * label + x % n, points[x]);
 
 }
