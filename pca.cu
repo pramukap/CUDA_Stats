6fbb7348a23c80b279a1803607562c9a38805708
pramukap <pramuka.perera@gmail.com>
2018-11-19 14:57:24 -0600

    Added random number generator (related to approx. eigenvalues)

--- pca.cu
+++ pca.cu
@@ -1,4 +1,6 @@
 #include "cuda_runtime.h"
+#include "curand_kernel.h"
+#include <time.h>
 #include <stdio.h>
 
 //Ax is the number of rows
@@ -43,16 +45,51 @@ __global__ void calcCovMatrix(double *A, double *cov, int Ax, int Ay) {
 }	
 
 //https://en.wikipedia.org/wiki/Principal_component_analysis#Covariance-free_computation
-__global__ void genRandVector(double *r) {
+//random vector that is of length num_features
+__global__ void genRandVector(double *r, curandState *state, unsigned long *clock_seed) {
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
-
-	r[x] = 
+	
+	unsigned long seed = 132241684309342543 + *clock_seed;
+	curand_init (seed, x, 0, &state[x]); 
+	
+	curandState localState = state[x];
+	r[x] = (double)curand_uniform(&localState);
+	state[x] = localState;
 }
 
-__global__ void findEigenVectors( 
+__global__ void findEigenVectors(int iterations) { 
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 
 	int i;
 	for (i = 0; i < iterations; i++) {
 		
 	}
+}
+
+#define SIZE 5
+
+int main() {
+	double r[5] = {0, 0, 0, 0, 0};		
+	unsigned long clock_seed = (unsigned long)clock();
+
+	double *r_d;
+	curandState *state_d;
+	unsigned long *clock_seed_d;
+
+	cudaMalloc((void**)&r_d, SIZE * sizeof(double));
+	cudaMalloc((void**)&state_d, SIZE * sizeof(curandState));
+	cudaMalloc((void**)&clock_seed_d, sizeof(unsigned long));
+
+	cudaMemcpy(r_d, r, SIZE * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(clock_seed_d, &clock_seed, sizeof(unsigned long), cudaMemcpyHostToDevice);
+
+	genRandVector<<<5, 1>>>(r_d, state_d, clock_seed_d);
+
+    cudaMemcpy(r, r_d, SIZE * sizeof(double), cudaMemcpyDeviceToHost);
+
+	int i;
+	for (i = 0; i < SIZE; i++) {
+		printf("%f ", r[i]);
+	}
+	printf("\n");
+}
