2e4df6531d3c956f6f0b59709ef3d4e6b3c13696
pramukap <pramuka.perera@gmail.com>
2018-11-20 03:55:35 -0600

    Debugging pca, fixed typo in matrixFunctions.cuh

--- matrixFunctions.cuh
+++ matrixFunctions.cuh
@@ -4,8 +4,8 @@
 
 __global__ void MatrixAdd(double * A, double * B, double * C);
 __device__ void MatrixSMul(double * A, double * B, double scalar);
-__global__ void MatrixTranspose(double * A, double * B, int Ax, int Ay)
-__global__ void MatrixMul(double * A, double * B, double * C, int Ax, int Ay, int Bx, int By)
+__global__ void MatrixTranspose(double * A, double * B, int Ax, int Ay);
+__global__ void MatrixMul(double * A, double * B, double * C, int Ax, int Ay, int Bx, int By);
 __global__ void AppendOne(double* src, double* dst, int num_row, int num_col);
 __global__ void AddLambdaToDiagonal(double * A, double lambda, int Ax, int Ay);
 __global__ void ExtractInverse(double *src, double* dst, int num_row, int num_col);
